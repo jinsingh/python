e61a9e583fde07ef12eaecf07969a6db2166d27e
jin <jin@devops.site>
2014-08-09 22:52:34 +0100

    Improved dal and adding listbox

---
+++ src/dal.py
@@ -0,0 +1,20 @@
+'''
+Created on 8 Aug 2014
+
+@author: jin
+'''
+import random
+
+def dalfunc():
+
+    in_file = open("text.txt").read().splitlines()
+    myline = random.choice(in_file)
+
+    words = myline.split()
+    wordslen = len(words)
+    random.shuffle(words)
+
+#for i in words:
+#   print i
+     
+    return wordslen, words
--- src/firstmodule.py
+++ src/firstmodule.py
@@ -5,23 +5,45 @@ Created on 6 Aug 2014
 '''
 import random
 import Tkinter
+from Tkinter import Tk, Frame, Button, Listbox, BOTH
+from dal import dalfunc
+
+# #Create Window frame
+class Example(Frame):
+    def __init__(self, parent):
+        Frame.__init__(self, parent, background="white")   
+        self.parent = parent        
+        self.initUI()
+     
+    def initUI(self):
+        self.parent.title("Gurbani Brain Training")
+        self.pack(fill=BOTH, expand=1)
+        QuitButton = Button(self, text ="Quit", command=self.quit)
+        QuitButton.place(x=50, y=50)
+        
+        (a1, a2) = dalfunc()
+        
+        #ListBox1 = Listbox()
+        
+        for i in a2:
+            print i
+            
+        #ListBox1.
+        
 
 
-# labl = Tkinter.label(text = "See me")
-# labl.pack()
-# labl.mainloop()
+# #Set widgets in frame
+def main():
+    root = Tkinter.Tk()
+    root.geometry("750x450+300+300")
+    app = Example(root)
+    app.mainloop()
+    #root.mainloop()
+ 
+if __name__ == '__main__':
+    main() 
 
-m = Tkinter.Tk()
-m.mainloop()
 
 
-in_file = open("text.txt").read().splitlines()
-myline = random.choice(in_file)
-
-words = myline.split()
-wordslen = len(words)
-random.shuffle(words)
-
-for i in words:
-    print i
-
+        #for i in a1:
+         #   print i
